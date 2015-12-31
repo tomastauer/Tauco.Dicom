749ea5247ff6f47ff43fc532534392212010a83d
TomikT <tauer.t@windowslive.com>
2015-12-31 17:26:56 +0100

    Add untracked files

---
+++ Tests/Tauco.Dicom.Abstraction.FellowOak.Tests.Integration/FellowOakDicomdirFileParserTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+using Castle.Windsor;
+
+using NUnit.Framework;
+
+namespace Tauco.Dicom.Abstraction.FellowOak.Tests.Integration
+{
+    [TestFixture]
+    public class FellowOakDicomdirFileParserTests
+    {
+        private IDicomdirFileParser mDicomdirFileParser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var container = new WindsorContainer().Install(new CommonInstaller());
+            mDicomdirFileParser = container.Resolve<IDicomdirFileParser>();
+        }
+
+
+        [Test]
+        public async void ParseDicomdir_PatientsAreParsedCorrectly()
+        {
+            // Arrange + Act
+            var result = await mDicomdirFileParser.ParseDicomdirAsync("Assets/DICOMDIR");
+            var patient = result.Patients.Single();
+            var study = result.Studies.Single();
+            var series = result.Series.Single();
+
+            // Assert
+            Assert.That(patient.PatientName.ToString(), Is.EqualTo("John Doe"));
+            Assert.That(patient.PatientID.StringRepresentationWithoutSlash, Is.EqualTo("9107256444"));
+            Assert.That(study.StudyInstanceUID.StringRepresentation, Is.EqualTo("1.2"));
+            Assert.That(series.StudyInstanceUID.StringRepresentation, Is.EqualTo("1.2"));
+            Assert.That(series.SeriesInstanceUID.StringRepresentation, Is.EqualTo("1.3"));
+        }
+    }
+}
---
+++ Tests/Tauco.Dicom.Abstraction.FellowOak.Tests.Integration/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d6ab70d1-be94-4c5b-863a-402f93763b26")]
